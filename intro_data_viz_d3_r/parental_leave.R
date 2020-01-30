1b6a67cff19bb59f8c133fc3ff1974f293c17a91
Dan Spector <danno2423@gmail.com>
2020-01-29 21:15:37 -0500

    2_r_import_data

---
+++ intro_data_viz_d3_r/parental_leave.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+
+worker_productivity <- read_csv('./data/worker_productivity.csv')
+parental_leave <- read_csv('./data/oecd_parental_leave.csv')
+country_codes <- read_csv('./data/countries_codes_and_coordinates.csv')
